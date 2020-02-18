cebd19b07b3af9eab8633ade6857d168836d6d6e
Insaf <hamzin730insaf@gmail.com>
2020-02-17 21:56:14 -0500

    initial project structure

---
+++ src/components/app/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './app.css';
+import { withBookStoreService } from '../hoc';
+
+const App = ( {bookstoreService} ) => {
+    console.log(bookstoreService.getBooks());
+    return <div>App</div>;
+}
+
+export default withBookStoreService()(App);
